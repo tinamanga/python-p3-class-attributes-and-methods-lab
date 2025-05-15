f8a8c758b624195a2de523e564198c4d5790d80a
tinamanga <tinamanga@gmail.com>
2025-05-16 00:20:48 +0300

    Use class attributes and methods to write durable and powerful code. Store and access song data using class attributes and methods. Accomplish complex programming tasks using knowledge from previous modules.

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,58 @@
 class Song:
-    pass
+     # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class-level data
+        self.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre=None):
+        genre = genre or cls.last_instance.genre
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist=None):
+        artist = artist or cls.last_instance.artist
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls):
+        genre = cls.last_instance.genre
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls):
+        artist = cls.last_instance.artist
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    # Store last instance so class methods can access instance data
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        Song.last_instance = self
+ 
